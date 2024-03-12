f9ad4d5cfc98d5820e0d4d2005d3885a3f35531d
Krzysztof Bicz <kristofarer@gmail.com>
2024-03-12 11:31:26 +0100

    First commit

---
+++ src/main/java/com/FlightAlright/FlightAlright/FlightAlrightApplication.java
@@ -0,0 +1,13 @@
+package com.FlightAlright.FlightAlright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAlrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAlrightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlightAlright/FlightAlright/FlightAlrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlightAlright.FlightAlright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAlrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
